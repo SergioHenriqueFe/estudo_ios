3fc71c63198ef97e100bc587bb0de28b25047b1f
sergioHEFE <121230365+SergioHenriqueFe@users.noreply.github.com>
2023-06-21 08:49:01 -0300

    Estrutura de repetição

---
+++ EstruturaDeRepetição/EstruturaDeRepetição/main.swift
@@ -0,0 +1,51 @@
+//
+//  main.swift
+//  EstruturaDeRepetição
+//
+//  Created by Sergio henrique Ferreira on 21/06/23.
+//
+
+import Foundation
+// MARK: Estrutura De Repetição
+
+//MARK:  While
+
+var index:Int = 0
+
+//enquanto index for <= 10 ele executa
+
+while index <= 10 {
+//    print("index: \(index)")
+    index  = index + 1
+}
+//print("valor do indice final é : \(index)")
+
+
+// MARK: Repeat
+
+var indexrepeat: Int = 1
+
+repeat {
+//    print("index: \(indexrepeat)")
+    indexrepeat += 1
+} while indexrepeat <= 10
+
+
+// MARK: For
+// depois do in tem que ser uma sequência de números , array
+
+for value in  10...50{
+    print(value)
+}
+
+
+
+
+
+
+
+
+
+
+
+
