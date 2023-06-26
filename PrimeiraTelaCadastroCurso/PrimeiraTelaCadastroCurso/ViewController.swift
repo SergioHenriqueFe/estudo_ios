20d79bca2e874687a68639f98615dc84a8e70a00
sergioHEFE <121230365+SergioHenriqueFe@users.noreply.github.com>
2023-06-26 08:23:59 -0300

    UITextFieldDelegate

--- PrimeiraTelaCadastroCurso/PrimeiraTelaCadastroCurso/ViewController.swift
+++ PrimeiraTelaCadastroCurso/PrimeiraTelaCadastroCurso/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
     @IBOutlet weak var nameTextField: UITextField!
     
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     
     @IBOutlet weak var registerButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,11 +25,40 @@ class ViewController: UIViewController {
         emailTextField.keyboardType = .emailAddress
         passwordTextField.keyboardType = .default
         passwordTextField.isSecureTextEntry = true
+       
+        // é para delegar caracteristicas para outros textfields para isso é necessário fazer em baixo extension viewController : UITextfieldDelegate
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
+    
+    
     @IBAction func tappedRegisterButton(_ sender: UIButton) {
         
     }
     
+}
+
+extension ViewController : UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        //ao inicializar o textfield
+        //para colocar um borda em volta do textfield
+        textField.layer.borderColor = UIColor.blue.cgColor
+        textField.layer.borderWidth = 1
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //é para quando o teclado desaparece
+        //quando o teclado abaixar voltar para borda =
+        textField.layer.borderWidth = 0
+}
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //ao clicar no return irá fazer algo
+        //para resignfirstResponder() é para abaixar o teclado do celular
+        textField.resignFirstResponder()
+    }
 }
 
