b850694e98b80cf4e6157d04f52e1edc190115f6
sergioHEFE <121230365+SergioHenriqueFe@users.noreply.github.com>
2023-09-14 22:17:34 -0300

    fazendo uso do naitvo usando URLSession para acessar API

--- SalaDeTeste/AulaCollectionView - alomofire/AulaCollectionView/Home/ViewModel/HomeViewModel.swift
+++ SalaDeTeste/AulaCollectionView - alomofire/AulaCollectionView/Home/ViewModel/HomeViewModel.swift
@@ -33,25 +33,25 @@ class HomeViewModel {
     }
     
     
-
+    
     //a forma a baixo é inserindo o dado manual
-//    private var posts = [
-//        Posts(profileImage: "img2", userName: "jack", postImage: "post1"),
-//        Posts(profileImage: "img4", userName: "samuel", postImage: "post2"),
-//        Posts(profileImage: "img3", userName: "jaJck", postImage: "post3"),
-//        Posts(profileImage: "img6", userName: "2jack", postImage: "post4"),
-//    
-//    ]
+    //    private var posts = [
+    //        Posts(profileImage: "img2", userName: "jack", postImage: "post1"),
+    //        Posts(profileImage: "img4", userName: "samuel", postImage: "post2"),
+    //        Posts(profileImage: "img3", userName: "jaJck", postImage: "post3"),
+    //        Posts(profileImage: "img6", userName: "2jack", postImage: "post4"),
+    //
+    //    ]
     // para passar a lista acima de privado para public
-//    private var story = [
-//        Stories(image: "img1", userName: "add Story"),
-//        Stories(image: "img2", userName: "jack"),
-//        Stories(image: "img3", userName: "carolina"),
-//        Stories(image: "img4", userName: "samuel"),
-//        Stories(image: "img5", userName: "ariana josep"),
-//        Stories(image: "img6", userName: "puggy_101"),
-//        Stories(image: "img7", userName: "dheeraj_ks")
-//    ]
+    //    private var story = [
+    //        Stories(image: "img1", userName: "add Story"),
+    //        Stories(image: "img2", userName: "jack"),
+    //        Stories(image: "img3", userName: "carolina"),
+    //        Stories(image: "img4", userName: "samuel"),
+    //        Stories(image: "img5", userName: "ariana josep"),
+    //        Stories(image: "img6", userName: "puggy_101"),
+    //        Stories(image: "img7", userName: "dheeraj_ks")
+    //    ]
     // para o retorno -- > tipo da variavel computada.
     
     
@@ -69,25 +69,40 @@ class HomeViewModel {
     
     public func fetchAllRequest(){
         //Usando Mock
-//        service.getHomeDataJson { homeData, error in
-//            if error == nil{
-//                self.posts = homeData?.posts ?? []
-//                self.story = homeData?.stories ?? []
-//            }
-//        }
-        service.getHomeDataAlamofire { homeData, error in
-            if error == nil{
+        //        service.getHomeDataJson { homeData, error in
+        //            if error == nil{
+        //                self.posts = homeData?.posts ?? []
+        //                self.story = homeData?.stories ?? []
+        //            }
+        //        }
+        
+        // usando Alomofire
+        //        service.getHomeDataAlamofire { homeData, error in
+        //            if error == nil{
+        //                self.posts = homeData?.posts ?? []
+        //                self.story = homeData?.stories ?? []
+        //                //usando o Protocol e assinando ele na HOMEVC
+        //                self.delegate?.success()
+        //
+        //            }else{
+        //                self.delegate?.error()
+        //            }
+        //
+        //        }
+        //    }
+        
+        service.getHomeDatUrlSession{homeData, error in
+            if error == nil {
                 self.posts = homeData?.posts ?? []
                 self.story = homeData?.stories ?? []
-                //usando o Protocol e assinando ele na HOMEVC
                 self.delegate?.success()
-                
-            }else{
+            }else {
                 self.delegate?.error()
             }
             
+            
         }
+        
+        
     }
-    
-    
 }
--- SalaDeTeste/AulaCollectionView - alomofire/AulaCollectionView/Service/HomeService.swift
+++ SalaDeTeste/AulaCollectionView - alomofire/AulaCollectionView/Service/HomeService.swift
@@ -7,9 +7,48 @@
 
 import UIKit
 import Alamofire
+//criação de um enum para o nativo (urlSession)
+
+enum ErrorDetail: Swift.Error{
+    case errorURL(urlString : String)
+    case detailError(datail : String)
+}
 
 class HomeService {
-    // assincrona, não sabe quando irá obter uma resposta 
+    //MARK: Método nativo
+    func getHomeDatUrlSession(completion:@escaping (HomeData?,Error?)->Void){
+        let urlString: String = "https://run.mocky.io/v3/dfc6e51a-3e44-4199-a4e5-909eda933e19"
+        //uso do url ela retorna como opc então é usado o guard
+        
+        guard let url : URL = URL(string: urlString)else{ return completion(nil, ErrorDetail.errorURL(urlString: urlString))}
+        var request = URLRequest(url:url)
+        request.httpMethod = "GET"
+        
+        //quase sempre é a mesma coisa, alterando a class (receita de bolo)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let dataResult = data else {return completion(nil, ErrorDetail.detailError(datail: "Error Data"))}
+            let json = try? JSONSerialization.jsonObject(with: dataResult,options: [])
+            print(json as Any)
+            
+            guard  let response = response as? HTTPURLResponse else {return}
+            if response.statusCode == 200{
+                do {
+                    let homeData : HomeData = try JSONDecoder().decode(HomeData.self,from: dataResult)
+                 completion(homeData,nil)
+                } catch  {
+                    completion(nil,error)
+                }
+                
+            } else {
+                completion (nil, error)
+                
+            }
+        }
+        
+        // para suspender
+        task.resume()
+    }
+    // assincrona, não sabe quando irá obter uma resposta
     func getHomeDataAlamofire(completion: @escaping (HomeData?, Error?) -> Void) {
         let url: String = "https://run.mocky.io/v3/dfc6e51a-3e44-4199-a4e5-909eda933e19"
         //get acesso, trazer informações
